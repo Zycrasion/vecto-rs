4acb9370eb7843285776371975501011ac2becce
Zycrasion <hendrix.may@gmail.com>
2023-08-20 21:17:25 +1000

    added add, point_inside and fixed ownership issues

--- src/quadtree.rs
+++ src/quadtree.rs
@@ -1,3 +1,5 @@
+use std::rc::Rc;
+
 use crate::{Vec2, bounding_box::AABB};
 
 pub type QuadValue<T> = (Vec2, T);
@@ -13,7 +15,7 @@ pub struct QuadTree<T>
     bl : Option<Box<QuadTree<T>>>,
 }
 
-impl<T> QuadTree<T>
+impl<T: Clone> QuadTree<T>
 {
     pub fn new(x : f32, y : f32, w : f32, h : f32, max_values: usize) -> Self
     {
@@ -29,5 +31,64 @@ impl<T> QuadTree<T>
             bl: None,
             max_values
         }
+    }
+
+    fn subdivide(&mut self)
+    {
+        self.tl = Some(Box::new(QuadTree::new(self.bb.x                  , self.bb.y                  , self.bb.w / 2.0, self.bb.h / 2.0, self.max_values)));
+        self.tr = Some(Box::new(QuadTree::new(self.bb.x + self.bb.w / 2.0, self.bb.y                  , self.bb.w / 2.0, self.bb.h / 2.0, self.max_values)));
+        self.bl = Some(Box::new(QuadTree::new(self.bb.x                  , self.bb.y + self.bb.h / 2.0, self.bb.w / 2.0, self.bb.h / 2.0, self.max_values)));
+        self.br = Some(Box::new(QuadTree::new(self.bb.x + self.bb.w / 2.0, self.bb.y + self.bb.h / 2.0, self.bb.w / 2.0, self.bb.h / 2.0, self.max_values)));
+        
+        let values : Vec<_> = self.values.drain(0..self.values.len()).collect();
+        for v in &values
+        {
+            let v1 = v.1.clone();
+            let p = v.0;
+            self.add(v1, p);
+        }
+    
+    }
+
+    fn point_inside(&self, p : Vec2) -> bool
+    {
+        self.bb.point_inside(p)
+    }
+
+    pub fn add(&mut self, v : T, p : Vec2)
+    {
+        if self.is_leaf()
+        {
+            self.values.push((p, v));
+
+            if self.values.len() > self.max_values
+            {
+                self.subdivide()
+            }
+
+        } else 
+        {
+            if self.tl.as_mut().unwrap().point_inside(p)
+            {
+                return self.tl.as_mut().unwrap().add(v, p);
+            }
+            if self.tr.as_mut().unwrap().point_inside(p)
+            {
+                return self.tr.as_mut().unwrap().add(v, p);
+            }
+            if self.bl.as_mut().unwrap().point_inside(p)
+            {
+                return self.bl.as_mut().unwrap().add(v, p);
+            }
+            if self.br.as_mut().unwrap().point_inside(p)
+            {
+                return self.br.as_mut().unwrap().add(v, p);
+            }
+        }
+    }
+
+    fn is_leaf(&self) -> bool
+    {
+        return self.tr.is_some()
     }
 }
