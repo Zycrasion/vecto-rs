265cb9d1254c4d7cb13e21e2d69b6361716a26bf
Mars Kitten <57377187+MarsKittenPro@users.noreply.github.com>
2023-08-21 09:41:15 +1000

    bumped version, added more functionality

--- src/bounding_box.rs
+++ src/bounding_box.rs
@@ -11,6 +11,17 @@ pub struct AABB
 
 impl AABB
 {
+    pub fn new(start : Vec2, size : Vec2) -> Self
+    {
+        AABB
+        {
+            x : start.0,
+            y : start.1,
+            w : size.0,
+            h : size.1
+        }
+    }
+
     pub fn point_inside(&self, p : Vec2) -> bool
     {
         return (p.0 >= self.x && p.0 <= self.x + self.w) && (p.1 >= self.y && p.1 <= self.y + self.h);
--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,8 @@
 mod vec2;
 pub use vec2::Vec2;
 
-// Not all features are implemented, so its a private crate feature
 mod bounding_box;
+pub use bounding_box::AABB;
 
 mod quadtree;
 pub use quadtree::QuadTree;
