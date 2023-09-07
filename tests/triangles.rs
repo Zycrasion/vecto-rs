67da551ded3c6f8f4fdf9e1996f145cf4049013c
Mars Kitten <57377187+MarsKittenPro@users.noreply.github.com>
2023-09-07 12:41:29 +1000

    tests

---
+++ tests/triangles.rs
@@ -0,0 +1,28 @@
+use vecto_rs::{maths::Triangle2D, Vec2};
+
+fn tri_test_inside(tri : Triangle2D, point : Vec2, expected : bool)
+{
+    assert_eq!(tri.point_inside_triangle(point), expected);
+}
+
+#[test]
+fn tri_test_inside1()
+{
+    tri_test_inside(Triangle2D::new(Vec2(0.0,0.0), Vec2(0.0,10.0), Vec2(10.0,0.0)), Vec2(5.0, 2.5), true);
+}
+
+fn tri_test_barycentric(tri : Triangle2D, point : Vec2, expected : (f32, f32, f32))
+{
+    assert_eq!(tri.barycentric_coordinates(point).unwrap(), expected)
+}
+
+#[test]
+fn tri_test_barycentric1()
+{
+    tri_test_barycentric(Triangle2D::new(Vec2(0.0,0.0), Vec2(0.0,10.0), Vec2(10.0,0.0)), Vec2(5.0, 2.5), (0.25, 0.25, 0.5));
+    let coords = Triangle2D::new(Vec2(0.0,0.0), Vec2(0.0,10.0), Vec2(10.0,0.0)).barycentric_coordinates(Vec2(5.0, 2.5)).unwrap();
+    assert_eq!(
+        coords.0 + coords.1 + coords.2,
+        1.0
+    )
+}
