6822c176808970b37e2ebdfa855d4f62308733b7
Mars Kitten <57377187+MarsKittenPro@users.noreply.github.com>
2023-09-08 14:42:05 +1000

    documented bounding_box.rs

--- src/bounding_box.rs
+++ src/bounding_box.rs
@@ -1,4 +1,4 @@
-use crate::Vector;
+use crate::positional::Vector;
 
 /// An Axis Aligned Bounding Box
 /// 
@@ -35,12 +35,25 @@ impl AABB
     }
 
     /// Check if a Vector is inside the Box
+    /// ```
+    /// pub use vecto_rs::*;
+    /// let a = AABB::new(Vector::new2(0.0, 0.0),Vector::new2(5.0, 5.0));
+    /// assert!(a.point_inside(Vector::new2(1.0,1.0)));
+    /// ```
     pub fn point_inside(&self, p : Vector) -> bool
     {
-        return p >= self.start && p <= self.start + self.size;
+        return  (p.x >= self.start.x && p.x <= self.start.x + self.size.x) && 
+                (p.y >= self.start.y && p.y <= self.start.y + self.size.y) && 
+                (p.z >= self.start.z && p.z <= self.start.z + self.size.z) 
     }
 
     /// Check if another AABB is inside this one
+    /// ```
+    /// pub use vecto_rs::*;
+    /// let a = AABB::new(Vector::new2(0.0, 0.0),Vector::new2(5.0, 5.0));
+    /// let b = AABB::new(Vector::new2(2.5, 0.0),Vector::new2(5.0, 5.0));
+    /// assert!(a.intersection(b));
+    /// ```
     pub fn intersection(&self, o : AABB) -> bool
     {
         return  (self.start.x <= o.start.x + o.size.x && self.start.x + self.size.x >= o.start.x) &&
