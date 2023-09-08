9373e81fc18cbeda98e9c5ba15a9dad3f5b35c80
Mars Kitten <57377187+MarsKittenPro@users.noreply.github.com>
2023-09-08 14:43:40 +1000

    fixed imports

--- tests/quadtree.rs
+++ tests/quadtree.rs
@@ -1,11 +1,11 @@
-use vecto_rs::{QuadTree, Vector};
+use vecto_rs::{spatial::QuadTree, positional::Vector};
 
 #[test]
 fn quadtree_simple()
 {
     let mut tree : QuadTree<bool> = QuadTree::new(0.0, 0.0, 500.0, 500.0, 80, 0.0, 10);
     
-    for i in 0..10_000
+    for i in 0..100
     {
         tree.add((i % 2) == 0, Vector::new2((i % 500) as f32, (i % 500) as f32));
     }
@@ -18,7 +18,7 @@ fn quadtree_simple()
         println!("{}: {}", c.0, c.1);
     }
 
-    for i in 0..1000
+    for i in 0..100
     {
         tree.remove(Vector::new2((i % 500) as f32, (i % 500) as f32));
     }
