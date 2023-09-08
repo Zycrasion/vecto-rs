82167f59ca79a40f3723e18d4785f08cc45fc145
Mars Kitten <57377187+MarsKittenPro@users.noreply.github.com>
2023-09-08 14:42:46 +1000

    documented line.rs

--- src/line.rs
+++ src/line.rs
@@ -1,5 +1,9 @@
-use crate::Vector;
+use crate::positional::Vector;
 
+/// Determine if point is on a line or to the side
+/// 0 is on line
+/// -1 is on one side
+/// 1 is on the other
 pub fn edge_function(line : (Vector, Vector), point : Vector) -> f32
 {
     (point.x - line.0.x) * (line.1.y - line.0.y) - (point.y - line.0.y) * (line.1.x - line.0.x)
