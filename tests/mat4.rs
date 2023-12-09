85864c2472f8a389e9084a7df72e9d1ae98c43ff
Zycrasion <hendrix.may@gmail.com>
2023-12-09 14:51:18 +1100

    fixed test + bumped ver

--- tests/mat4.rs
+++ tests/mat4.rs
@@ -24,8 +24,8 @@ fn from_array_and_transpose()
         1., 5., 59., 6.,
     ];
     
-    let mat = Mat4::from_array(&array);
-    
+    let mat = Mat4::from_array(array);
+
     assert_eq!(mat.to_string(), "5 2 32 5 \n4 3 34 6 \n9 3 5 7 \n1 5 59 6 \n");
     assert_eq!(mat.transpose().to_string(), "5 4 9 1 \n2 3 3 5 \n32 34 5 59 \n5 6 7 6 \n")
 }
