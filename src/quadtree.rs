da45531872411f36eb749ba7b95dcf7d45c5d3cf
Zycrasion <hendrix.may@gmail.com>
2024-06-04 12:45:56 +1000

    modified test + finished insert implementation

--- src/quadtree.rs
+++ src/quadtree.rs
@@ -1,3 +1,5 @@
+use std::time::{Duration, Instant};
+
 use crate::linear::Vector;
 use crate::spatial::AABB;
 use crate::vec::VectorTrait;
@@ -6,7 +8,7 @@ use crate::Float;
 struct QuadBranch
 {
     branches : Option<Box<(QuadBranch, QuadBranch, QuadBranch, QuadBranch)>>,
-    contents : Vec<usize>
+    contents : Vec<(usize, Vector)>
 }
 
 impl QuadBranch
@@ -27,6 +29,9 @@ pub struct QuadTree
     root : QuadBranch,
     half_size : Vector,
     midpoint : Vector,
+    bb : AABB,
+    pub min_size : Vector,
+    pub max_items : usize
 }
 
 impl QuadTree
@@ -38,6 +43,106 @@ impl QuadTree
             root: QuadBranch::new(),
             midpoint : bb.start + bb.size / 2.,
             half_size : bb.size / 2.,
+            bb,
+            min_size : bb.size / 10.,
+            max_items : 100,
         }
+    }
+
+    fn insert_en_bulk(p : Vec<(usize, Vector)>, peeked_branch : &mut QuadBranch, midpoint : Vector)
+    {
+        for v in p
+        {
+            let normalised = v.1 - midpoint;
+            match (normalised.x.is_sign_positive(), normalised.y.is_sign_positive())
+            {
+                (true, true) => {
+                    peeked_branch.branches.as_mut().unwrap().0.contents.push(v);
+                },
+                (true, false) => {
+                    peeked_branch.branches.as_mut().unwrap().1.contents.push(v);
+                },
+                (false, false) => {
+                    peeked_branch.branches.as_mut().unwrap().2.contents.push(v);
+                }
+                (false, true) => {
+                    peeked_branch.branches.as_mut().unwrap().3.contents.push(v);
+                },
+            }
+        }
+    }
+
+    pub fn insert(&mut self, i : usize, v : &Vector) -> Result<(), ()>
+    {
+        if !self.bb.point_inside(*v)
+        {
+            return Err(())
+        }
+
+        let mut normalised : Vector;
+        let mut current_size = self.bb.size;
+        let mut current_midpoint = self.midpoint;
+        let mut currently_peeked_branch = &mut self.root;
+        let instant = Instant::now();
+        loop
+        {
+            if instant.elapsed() >= Duration::from_secs(5)
+            {
+                break;
+            }
+            if currently_peeked_branch.branches.is_none()
+            {
+                if currently_peeked_branch.contents.len() < self.max_items
+                {
+                    currently_peeked_branch.contents.push((i, *v));
+                    return Ok(())
+                }
+
+                // Subdivide the branch 
+                let saved_contents = currently_peeked_branch.contents.drain(0..currently_peeked_branch.contents.len());
+
+                currently_peeked_branch.branches = Some(Box::new((QuadBranch::new(),QuadBranch::new(),QuadBranch::new(),QuadBranch::new())));
+                Self::insert_en_bulk(saved_contents.collect(), currently_peeked_branch, self.midpoint);
+                return Ok(());
+            }
+            normalised = *v - self.midpoint;
+            match (normalised.x.is_sign_positive(), normalised.y.is_sign_positive())
+            {
+                (true, true) => {
+                    current_midpoint += current_size / 2.;
+                    current_size /= 2.;
+                    if currently_peeked_branch.branches.is_some()
+                    {
+                        currently_peeked_branch = &mut currently_peeked_branch.branches.as_mut().unwrap().0;
+                    }
+                },
+                (true, false) => {
+                    current_midpoint += current_size / Vector::new2(2., -2.);
+                    current_size /= 2.;
+                    if currently_peeked_branch.branches.is_some()
+                    {
+                        currently_peeked_branch = &mut currently_peeked_branch.branches.as_mut().unwrap().1;
+                    }
+                },
+                (false, false) => {
+                    current_midpoint += self.half_size / -2.;
+                    current_size /= 2.;
+                    if currently_peeked_branch.branches.is_some()
+                    {
+                        currently_peeked_branch = &mut currently_peeked_branch.branches.as_mut().unwrap().2;
+                    }
+                }
+                (false, true) => {
+                    current_midpoint += self.half_size / Vector::new2(-2., 2.);
+                    current_size /= 2.;
+                    if currently_peeked_branch.branches.is_some()
+                    {
+                        currently_peeked_branch = &mut currently_peeked_branch.branches.as_mut().unwrap().3;
+                    }
+                },
+            }
+        }
+
+        Err(())
     }
 }
--- tests/quadtree.rs
+++ tests/quadtree.rs
@@ -1,28 +1,29 @@
+use vecto_rs::spatial::AABB;
 use vecto_rs::{spatial::QuadTree, linear::*};
 use vecto_rs::Float;
 
 #[test]
 fn quadtree_simple()
 {
-    let mut tree : QuadTree<bool> = QuadTree::new(0.0, 0.0, 500.0, 500.0, 80, 0.0, 10);
+    let mut tree : QuadTree = QuadTree::new(AABB::new(Vector::new2(0.,0.), Vector::new2(500., 500.)));
     
     for i in 0..100
     {
-        tree.add((i % 2) == 0, Vector::new2((i % 500) as Float, (i % 500) as Float));
+        tree.insert(i, &Vector::new2((i % 500) as Float, (i % 500) as Float));
     }
 
-    let cell = tree.query(Vector::new2(230.0, 230.0));
+    // let cell = tree.query(Vector::new2(230.0, 230.0));
 
-    println!("{}", cell.len());
-    for c in cell
-    {
-        println!("{}: {}", c.0, c.1);
-    }
+    // println!("{}", cell.len());
+    // for c in cell
+    // {
+    //     println!("{}: {}", c.0, c.1);
+    // }
 
-    for i in 0..100
-    {
-        tree.remove(Vector::new2((i % 500) as Float, (i % 500) as Float));
-    }
+    // for i in 0..100
+    // {
+    //     tree.remove(Vector::new2((i % 500) as Float, (i % 500) as Float));
+    // }
 
-    tree.prune();
+    // tree.prune();
 }
