2aa4a5372b61c06c2df20fa1fc97e9e47334f08a
Zycrasion <hendrix.may@gmail.com>
2025-03-08 17:49:16 +1100

    Changed the types a bit to fix some bugs

--- src/types.rs
+++ src/types.rs
@@ -1,6 +1,7 @@
+use core::f32;
 use std::{fmt::Debug, ops::{AddAssign, DivAssign, MulAssign, RemAssign, SubAssign}};
 
-use num_traits::{Float, Num};
+use num_traits::{Float, FloatConst, Num, NumCast};
 
 macro_rules! impl_number {
     ($($number_type:tt )+) => {
@@ -24,9 +25,17 @@ pub trait BaseNumber :
 
 pub trait BaseFloat : 
     BaseNumber +
-    Float
-{}
+    Float + 
+    FloatConst +
+    NumCast
+{
+}
 
 impl_number!(i8 i16 i32 i64);
 impl_number!(u8 u16 u32 u64);
 impl_number!(f32 f64);
+
+impl<T : Float + FloatConst + BaseNumber> BaseFloat for T
+{
+
+}
