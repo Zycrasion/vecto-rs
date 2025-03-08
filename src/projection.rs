683fec60d271e0558b56ace56a0c9c28ebf33bac
Zycrasion <hendrix.may@gmail.com>
2025-03-08 19:14:46 +1100

    can now create matrices

--- src/projection.rs
+++ src/projection.rs
@@ -2,7 +2,11 @@ use std::ops::Neg;
 
 use num_traits::{cast, NumCast};
 
-use crate::{angle::Angle, mat4::Mat4, types::BaseNumber};
+use crate::{
+    angle::Angle,
+    mat4::Mat4,
+    types::{BaseFloat, BaseNumber},
+};
 
 #[derive(Debug, Default, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
 pub struct OrthographicProjection<N> {
@@ -14,7 +18,14 @@ pub struct OrthographicProjection<N> {
     pub far: N,
 }
 
-pub const fn ortho<N>(left: N, right: N, top: N, bottom: N, near: N, far: N) -> OrthographicProjection<N> {
+pub const fn ortho<N>(
+    left: N,
+    right: N,
+    top: N,
+    bottom: N,
+    near: N,
+    far: N,
+) -> OrthographicProjection<N> {
     OrthographicProjection {
         left,
         right,
@@ -50,8 +61,7 @@ impl<N: BaseNumber + NumCast + Neg<Output = N>> OrthographicProjection<N> {
         let top = h_height;
         let bottom = -h_height;
 
-        Self
-        {
+        Self {
             left,
             right,
             top,
@@ -62,54 +72,73 @@ impl<N: BaseNumber + NumCast + Neg<Output = N>> OrthographicProjection<N> {
     }
 
     pub fn create_matrix(&self) -> Mat4<N> {
-        let two : N = cast(2).unwrap();
+        let two: N = cast(2).unwrap();
 
-        Mat4::new(
-            [
-                two / (self.right - self.left),
-                N::zero(),
-                N::zero(),
-                N::zero(),
-                N::zero(),
-                two / (self.top - self.bottom),
-                N::zero(),
-                N::zero(),
-                N::zero(),
-                N::zero(),
-                N::one() / (self.near - self.far),
-                N::zero(),
-                (self.right + self.left) / (self.left - self.right),
-                (self.top + self.bottom) / (self.bottom - self.top),
-                self.near / (self.near - self.far),
-                N::one(),
-            ]
-        )
+        Mat4::new([
+            two / (self.right - self.left),
+            N::zero(),
+            N::zero(),
+            N::zero(),
+            N::zero(),
+            two / (self.top - self.bottom),
+            N::zero(),
+            N::zero(),
+            N::zero(),
+            N::zero(),
+            N::one() / (self.near - self.far),
+            N::zero(),
+            (self.right + self.left) / (self.left - self.right),
+            (self.top + self.bottom) / (self.bottom - self.top),
+            self.near / (self.near - self.far),
+            N::one(),
+        ])
     }
 }
 
 #[derive(Debug, Default, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
 pub struct PerspectiveProjection<N> {
-    pub fovy : Angle<N>,
-    pub aspect : N,
-    pub near : N,
-    pub far : N,
+    pub fovy: Angle<N>,
+    pub aspect: N,
+    pub near: N,
+    pub far: N,
 }
 
-impl<N : BaseNumber> PerspectiveProjection<N>
-{
-    pub fn new(
-        fovy : Angle<N>,
-        aspect : N,
-        near : N,
-        far : N, 
-    ) -> Self
-    {
-        Self
-        {
+impl<N: BaseNumber> PerspectiveProjection<N> {
+    pub fn new(fovy: Angle<N>, aspect: N, near: N, far: N) -> Self {
+        Self {
             fovy,
             aspect,
             near,
             far,
         }
+    }
+}
+
+impl<N: BaseFloat> PerspectiveProjection<N> {
+    pub fn create_matrix(&self) -> Mat4<N> {
+        let two = cast(2).unwrap();
+        let f = (N::PI() / two - self.fovy.as_radians() / two).tan();
+        let range_inv = N::one() / (self.near - self.far);
+        Mat4::new([
+            f / self.aspect,
+            N::zero(),
+            N::zero(),
+            N::zero(),
+
+            N::zero(),
+            f,
+            N::zero(),
+            N::zero(),
+            
+            N::zero(),
+            N::zero(),
+            self.far * range_inv,
+            -N::one(),
+
+            N::zero(),
+            N::zero(),
+            self.far * self.near * range_inv,
+            N::zero(),
+        ])
     }
 }
