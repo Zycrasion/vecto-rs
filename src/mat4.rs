94f6d85d8bd0678008bcffbdb0fa66205c3727e8
Zycrasion <hendrix.may@gmail.com>
2025-03-08 18:32:25 +1100

    MATRIX  MULT

--- src/mat4.rs
+++ src/mat4.rs
@@ -1,4 +1,4 @@
-use std::ops::Neg;
+use std::ops::{Mul, Neg};
 
 use crate::types::{BaseFloat, BaseNumber};
 
@@ -163,9 +163,56 @@ impl<N: BaseNumber + Neg<Output = N>> Mat4<N> {
     }
 }
 
+impl<N: BaseNumber> Mul<Mat4<N>> for Mat4<N> {
+    type Output = Mat4<N>;
+
+    fn mul(self, rhs: Mat4<N>) -> Self::Output {
+        let l = self.contents;
+        let r = rhs.contents;
+        let mut contents = [N::zero(); 16];
+
+        contents[0] = l[0] * r[0] + l[4] * r[1] + l[8] * r[2] + l[12] * r[3];
+        contents[1] = l[1] * r[0] + l[5] * r[1] + l[9] * r[2] + l[13] * r[3];
+        contents[2] = l[2] * r[0] + l[6] * r[1] + l[10] * r[2] + l[14] * r[3];
+        contents[3] = l[3] * r[0] + l[7] * r[1] + l[11] * r[2] + l[15] * r[3];
+
+        contents[4] = l[0] * r[4] + l[4] * r[5] + l[8] * r[6] + l[12] * r[7];
+        contents[5] = l[1] * r[4] + l[5] * r[5] + l[9] * r[6] + l[13] * r[7];
+        contents[6] = l[2] * r[4] + l[6] * r[5] + l[10] * r[6] + l[14] * r[7];
+        contents[7] = l[3] * r[4] + l[7] * r[5] + l[11] * r[6] + l[15] * r[7];
+
+        contents[8] = l[0] * r[8] + l[4] * r[9] + l[8] * r[10] + l[12] * r[11];
+        contents[9] = l[1] * r[8] + l[5] * r[9] + l[9] * r[10] + l[13] * r[11];
+        contents[10] = l[2] * r[8] + l[6] * r[9] + l[10] * r[10] + l[14] * r[11];
+        contents[11] = l[3] * r[8] + l[7] * r[9] + l[11] * r[10] + l[15] * r[11];
+
+        contents[12] = l[0] * r[12] + l[4] * r[13] + l[8] * r[14] + l[12] * r[15];
+        contents[13] = l[1] * r[12] + l[5] * r[13] + l[9] * r[14] + l[13] * r[15];
+        contents[14] = l[2] * r[12] + l[6] * r[13] + l[10] * r[14] + l[14] * r[15];
+        contents[15] = l[3] * r[12] + l[7] * r[13] + l[11] * r[14] + l[15] * r[15];
+
+        Mat4::new(contents)
+    }
+}
+
 #[cfg(feature = "bytemuck")]
 use bytemuck::{Pod, Zeroable};
 #[cfg(feature = "bytemuck")]
 unsafe impl<N: Pod + Zeroable> Pod for Mat4<N> {}
 #[cfg(feature = "bytemuck")]
 unsafe impl<N: Pod + Zeroable> Zeroable for Mat4<N> {}
+
+#[cfg(test)]
+mod mat4 {
+    use super::Mat4;
+
+    #[test]
+    fn identity_mul() {
+        let identity: Mat4<f32> = Mat4::identity();
+        let abc = Mat4::new([ 0., 1., 5., 3., 10., 12., 19123., 16., -123., 11416534., 12312., 16., -123., 123., 4687567., 123., ]);
+    
+        assert_eq!(identity * abc, abc);
+        assert_eq!(abc * identity, abc);
+        assert_eq!(identity * identity, identity);
+    }
+}
