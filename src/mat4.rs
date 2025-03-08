dec93d8a6c43b02bf6aa9d7d3126509e8b8a3f93
Zycrasion <hendrix.may@gmail.com>
2025-03-08 18:14:39 +1100

    inverse of matrix *GULP*

--- src/mat4.rs
+++ src/mat4.rs
@@ -1,54 +1,168 @@
-use crate::types::BaseNumber;
+use crate::types::{BaseFloat, BaseNumber};
 
 #[repr(C)]
 #[derive(Debug, Default, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
-pub struct Mat4<N>
-{
-    contents : [N; 16]
+pub struct Mat4<N> {
+    contents: [N; 16],
 }
 
-impl<N> Mat4<N>
-{
-    pub fn new(
-        contents : [N; 16]
-    ) -> Self
-    {
-        Self
-        {
-            contents
-        }
+impl<N> Mat4<N> {
+    pub fn new(contents: [N; 16]) -> Self {
+        Self { contents }
     }
 }
 
-impl<N : Copy> Mat4<N>
-{
-    pub fn transpose(&self) -> Self
-    {
+impl<N: Copy> Mat4<N> {
+    pub fn transpose(&self) -> Self {
         Self::new([
-            self.contents[0], self.contents[4], self.contents[8], self.contents[12],
-            self.contents[1], self.contents[5], self.contents[9], self.contents[13],
-            self.contents[2], self.contents[6], self.contents[10], self.contents[14],
-            self.contents[3], self.contents[7], self.contents[11], self.contents[15],
+            self.contents[0],
+            self.contents[4],
+            self.contents[8],
+            self.contents[12],
+            self.contents[1],
+            self.contents[5],
+            self.contents[9],
+            self.contents[13],
+            self.contents[2],
+            self.contents[6],
+            self.contents[10],
+            self.contents[14],
+            self.contents[3],
+            self.contents[7],
+            self.contents[11],
+            self.contents[15],
         ])
     }
 }
 
-impl<N : BaseNumber> Mat4<N>
-{
-    pub fn identity() -> Mat4<N>
-    {
-        Self { contents: [
-            N::one(), N::zero(), N::zero(), N::zero(),
-            N::zero(), N::one(), N::zero(), N::zero(),
-            N::zero(), N::zero(), N::one(), N::zero(),
-            N::zero(), N::zero(), N::zero(), N::one(),
-        ] }
+impl<N: BaseFloat> Mat4<N> {
+    pub fn identity() -> Mat4<N> {
+        Self {
+            contents: [
+                N::one(),
+                N::zero(),
+                N::zero(),
+                N::zero(),
+                N::zero(),
+                N::one(),
+                N::zero(),
+                N::zero(),
+                N::zero(),
+                N::zero(),
+                N::one(),
+                N::zero(),
+                N::zero(),
+                N::zero(),
+                N::zero(),
+                N::one(),
+            ],
+        }
+    }
+
+    pub fn inverse(&self) -> Option<Mat4<N>> {
+        let m = self.contents;
+        let mut inv = [N::zero(); 16];
+
+        inv[0] = m[5] * m[10] * m[15] - m[5] * m[11] * m[14] - m[9] * m[6] * m[15]
+            + m[9] * m[7] * m[14]
+            + m[13] * m[6] * m[11]
+            - m[13] * m[7] * m[10];
+        inv[4] = -m[4] * m[10] * m[15] + m[4] * m[11] * m[14] + m[8] * m[6] * m[15]
+            - m[8] * m[7] * m[14]
+            - m[12] * m[6] * m[11]
+            + m[12] * m[7] * m[10];
+
+        inv[8] = m[4] * m[9] * m[15] - m[4] * m[11] * m[13] - m[8] * m[5] * m[15]
+            + m[8] * m[7] * m[13]
+            + m[12] * m[5] * m[11]
+            - m[12] * m[7] * m[9];
+
+        inv[12] = -m[4] * m[9] * m[14] + m[4] * m[10] * m[13] + m[8] * m[5] * m[14]
+            - m[8] * m[6] * m[13]
+            - m[12] * m[5] * m[10]
+            + m[12] * m[6] * m[9];
+
+        inv[1] = -m[1] * m[10] * m[15] + m[1] * m[11] * m[14] + m[9] * m[2] * m[15]
+            - m[9] * m[3] * m[14]
+            - m[13] * m[2] * m[11]
+            + m[13] * m[3] * m[10];
+
+        inv[5] = m[0] * m[10] * m[15] - m[0] * m[11] * m[14] - m[8] * m[2] * m[15]
+            + m[8] * m[3] * m[14]
+            + m[12] * m[2] * m[11]
+            - m[12] * m[3] * m[10];
+
+        inv[9] = -m[0] * m[9] * m[15] + m[0] * m[11] * m[13] + m[8] * m[1] * m[15]
+            - m[8] * m[3] * m[13]
+            - m[12] * m[1] * m[11]
+            + m[12] * m[3] * m[9];
+
+        inv[13] = m[0] * m[9] * m[14] - m[0] * m[10] * m[13] - m[8] * m[1] * m[14]
+            + m[8] * m[2] * m[13]
+            + m[12] * m[1] * m[10]
+            - m[12] * m[2] * m[9];
+
+        inv[2] = m[1] * m[6] * m[15] - m[1] * m[7] * m[14] - m[5] * m[2] * m[15]
+            + m[5] * m[3] * m[14]
+            + m[13] * m[2] * m[7]
+            - m[13] * m[3] * m[6];
+
+        inv[6] = -m[0] * m[6] * m[15] + m[0] * m[7] * m[14] + m[4] * m[2] * m[15]
+            - m[4] * m[3] * m[14]
+            - m[12] * m[2] * m[7]
+            + m[12] * m[3] * m[6];
+
+        inv[10] = m[0] * m[5] * m[15] - m[0] * m[7] * m[13] - m[4] * m[1] * m[15]
+            + m[4] * m[3] * m[13]
+            + m[12] * m[1] * m[7]
+            - m[12] * m[3] * m[5];
+
+        inv[14] = -m[0] * m[5] * m[14] + m[0] * m[6] * m[13] + m[4] * m[1] * m[14]
+            - m[4] * m[2] * m[13]
+            - m[12] * m[1] * m[6]
+            + m[12] * m[2] * m[5];
+
+        inv[3] = -m[1] * m[6] * m[11] + m[1] * m[7] * m[10] + m[5] * m[2] * m[11]
+            - m[5] * m[3] * m[10]
+            - m[9] * m[2] * m[7]
+            + m[9] * m[3] * m[6];
+
+        inv[7] = m[0] * m[6] * m[11] - m[0] * m[7] * m[10] - m[4] * m[2] * m[11]
+            + m[4] * m[3] * m[10]
+            + m[8] * m[2] * m[7]
+            - m[8] * m[3] * m[6];
+
+        inv[11] = -m[0] * m[5] * m[11] + m[0] * m[7] * m[9] + m[4] * m[1] * m[11]
+            - m[4] * m[3] * m[9]
+            - m[8] * m[1] * m[7]
+            + m[8] * m[3] * m[5];
+
+        inv[15] = m[0] * m[5] * m[10] - m[0] * m[6] * m[9] - m[4] * m[1] * m[10]
+            + m[4] * m[2] * m[9]
+            + m[8] * m[1] * m[6]
+            - m[8] * m[2] * m[5];
+
+        let det = m[0] * inv[0] + m[1] * inv[4] + m[2] * inv[8] + m[3] * inv[12];
+
+        if det == N::zero()
+        {
+            return None;
+        }
+
+        let det = N::one() / det;
+
+        for i in 0..16
+        {
+            inv[i] *= det;
+        }
+
+        Some(Self::new(inv))
     }
 }
 
-#[cfg(feature="bytemuck")]
+#[cfg(feature = "bytemuck")]
 use bytemuck::{Pod, Zeroable};
-#[cfg(feature="bytemuck")]
-unsafe impl<N : Pod + Zeroable> Pod for Mat4<N> {}
-#[cfg(feature="bytemuck")]
-unsafe impl<N : Pod + Zeroable> Zeroable for Mat4<N> {}
+#[cfg(feature = "bytemuck")]
+unsafe impl<N: Pod + Zeroable> Pod for Mat4<N> {}
+#[cfg(feature = "bytemuck")]
+unsafe impl<N: Pod + Zeroable> Zeroable for Mat4<N> {}
